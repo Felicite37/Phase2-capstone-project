6ea9945fd8c30a8874238ba7ff5f438fae831260
Felicite37 <feliciteniyo24@gmail.com>
2025-11-18 15:33:58 +0200

    create hooks folder and add files

---
+++ medium-capstone/src/hooks/use-toast.ts
@@ -0,0 +1,165 @@
+import * as React from "react";
+
+type ToasterToast = {
+  id: string;
+  title?: React.ReactNode;
+  description?: React.ReactNode;
+  action?: React.ReactNode;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+};
+
+const TOAST_LIMIT = 1;
+const TOAST_REMOVE_DELAY = 1000000;
+
+type ActionType =
+  | {
+      type: "ADD_TOAST";
+      toast: ToasterToast;
+    }
+  | {
+      type: "UPDATE_TOAST";
+      toast: Partial<ToasterToast>;
+    }
+  | {
+      type: "DISMISS_TOAST";
+      toastId?: string;
+    }
+  | {
+      type: "REMOVE_TOAST";
+      toastId?: string;
+    };
+
+interface State {
+  toasts: ToasterToast[];
+}
+
+const actionTypes = {
+  ADD_TOAST: "ADD_TOAST",
+  UPDATE_TOAST: "UPDATE_TOAST",
+  DISMISS_TOAST: "DISMISS_TOAST",
+  REMOVE_TOAST: "REMOVE_TOAST",
+};
+
+let count = 0;
+
+function genId() {
+  count = (count + 1) % Number.MAX_SAFE_INTEGER;
+  return count.toString();
+}
+
+export function useToast() {
+  const [state, dispatch] = React.useReducer(toastReducer, {
+    toasts: [],
+  });
+
+  function toast(props: ToasterToast) {
+    const id = genId();
+
+    const update = (props: ToasterToast) =>
+      dispatch({
+        type: "UPDATE_TOAST",
+        toast: { ...props, id },
+      });
+    const dismiss = () => dispatch({ type: "DISMISS_TOAST", toastId: id });
+
+    dispatch({
+      type: "ADD_TOAST",
+      toast: {
+        ...props,
+        id,
+        open: true,
+        onOpenChange: (open: boolean) => {
+          if (!open) dismiss();
+        },
+      },
+    });
+
+    return {
+      id: id,
+      dismiss,
+      update,
+    };
+  }
+
+  function toastAction(
+    action: React.ReactNode,
+    title: string,
+    description?: string
+  ) {
+    toast({
+      title,
+      description,
+      action,
+    });
+  }
+
+  return {
+    toast,
+    toastAction,
+    toasts: state.toasts,
+  };
+}
+
+function toastReducer(state: State, action: ActionType): State {
+  switch (action.type) {
+    case "ADD_TOAST":
+      return {
+        ...state,
+        toasts: [action.toast, ...state.toasts].slice(0, TOAST_LIMIT),
+      };
+
+    case "UPDATE_TOAST":
+      return {
+        ...state,
+        toasts: state.toasts.map((t) =>
+          t.id === action.toast.id ? { ...t, ...action.toast } : t
+        ),
+      };
+
+    case "DISMISS_TOAST": {
+      const { toastId } = action;
+
+      if (toastId) {
+        setTimeout(() => {
+          dispatch({
+            type: "REMOVE_TOAST",
+            toastId,
+          });
+        }, TOAST_REMOVE_DELAY);
+      } else {
+        state.toasts.forEach((toast) => {
+          setTimeout(() => {
+            dispatch({
+              type: "REMOVE_TOAST",
+              toastId: toast.id,
+            });
+          }, TOAST_REMOVE_DELAY);
+        });
+      }
+
+      return {
+        ...state,
+        toasts: state.toasts.map((t) =>
+          t.id === toastId || toastId === undefined
+            ? {
+                ...t,
+                open: false,
+              }
+            : t
+        ),
+      };
+    }
+    case "REMOVE_TOAST":
+      if (action.toastId === undefined) {
+        return {
+          ...state,
+          toasts: [],
+        };
+      }
+      return {
+        ...state,
+        toasts: state.toasts.filter((t) => t.id !== action.toastId),
+      };
+  }
+}
